d6a0fbdb0084a26eae6b22518ad20025a8759458
MohammedFathi <mohamed@easy-trademarks.com>
2022-06-01 17:11:21 +0200

    Upload to do app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {useState} from "react";
 
 function App() {
+  const [todoValue, setTodoValue] = useState("");
+  const [todoList, setTodoList] = useState([]);
+  const handlInputChange = event => {
+    setTodoValue(event.target.value);
+  };
+
+  const handleSubmit = () => {
+    setTodoList(prevTodoValue =>
+      prevTodoValue.concat({ value: todoValue })
+    );
+    setTodoValue("");
+  };
+
+  const handleCheckBoxClick = (listnumber, event) => {
+    setTodoList(todoList.map(
+        (item, index) =>
+          {return{
+            ...item,
+            isChecked: index === listnumber ? event.currentTarget.checked : item.isChecked
+          }}
+      
+    ))
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input type="text" value={todoValue} onChange={handlInputChange} />
+      <button onClick={handleSubmit}>submmit</button>
+      {todoList.map(
+        (item, index) =>
+          item.value && (
+            <li>
+              <input
+                type="checkbox"
+                onClick={handleCheckBoxClick.bind(null, index)}
+              />
+              {
+                item.isChecked ? <del>{item.value}</del> : item.value
+              }
+            </li>
+          )
+      )}
     </div>
   );
 }
